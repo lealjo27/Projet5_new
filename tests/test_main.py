88d92bf0ed7675824814d1ae3b3153e4c9a3367e
lealjo27 <lealjo27@gmail.com>
2026-05-19 12:50:08 +0200

    feat: ajout des premiers tests

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200 
+
+def test_placeholder():
+    assert 1 +1 == 2
